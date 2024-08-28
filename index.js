defbca31ef602c3bbc44dafabf80d17bfe2f7df0
Dylan Tin <dylant711@gmail.com>
2024-08-28 14:06:08 -0400

    finished filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+// 1st Test
+function findMatching(driversArray,driverName){
+    //const driverName = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+    const result = driversArray.filter(name => {
+        return name.toUpperCase() === driverName.toUpperCase()
+});
+    return result;
+}
+
+// 2nd Test
+function fuzzyMatch(driversArray,letter){
+    return driversArray.filter(driversArray => 
+        driversArray.startsWith(letter));
+}
+
+// 3rd Test
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
